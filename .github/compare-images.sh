6cd776660dc818c88bbd7e4bcd362d31490c85d4
DenysSirchenko <denys.sirchenko@spryker.com>
2025-04-10 22:50:40 +0300

    Updated .github/compare-images.sh

--- .github/compare-images.sh
+++ .github/compare-images.sh
@@ -18,4 +18,48 @@ docker run --rm "$IMAGE_TAG" bash -c '
   else
     echo "Unknown"
   fi
+
+  echo ""
+  echo "=== OS Release ==="
+  if [ -f /etc/os-release ]; then
+    cat /etc/os-release
+  else
+    echo "Unknown"
+  fi
+
+  echo ""
+  echo "=== Installed APT Packages ==="
+  if command -v dpkg &> /dev/null; then
+    dpkg -l | awk '\''{ print $2 " " $3 }'\'' | sort
+  elif command -v apk &> /dev/null; then
+    apk info -vv
+  else
+    echo "Package manager not found"
+  fi
+
+  echo ""
+  echo "=== Java Version ==="
+  java -version 2>&1 || echo "Java not installed"
+
+  echo ""
+  echo "=== Python Packages ==="
+  if command -v pip &> /dev/null; then
+    pip list
+  elif command -v pip3 &> /dev/null; then
+    pip3 list
+  else
+    echo "pip not installed"
+  fi
+
+  echo ""
+  echo "=== Environment Variables ==="
+  printenv | sort
+
+  echo ""
+  echo "=== Installed Jenkins Plugins (if any) ==="
+  if [ -d /usr/share/jenkins/ref/plugins ]; then
+    ls -1 /usr/share/jenkins/ref/plugins
+  else
+    echo "Plugin folder not found"
+  fi
 '
